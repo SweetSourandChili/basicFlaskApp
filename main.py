48ea47b0e4c1018a7e35c1f5b56bbf23e47a853b
Arda Meriç <ardam416@gmail.com>
2023-04-18 15:48:53 +0300

    c3

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask
 import os
+import ssl
 from pymongo import MongoClient
+
+# Retrieve the MongoDB Atlas URI from the environment variables
+mongo_uri = os.environ['MONGO_URI']
+
 app = Flask(__name__)
+client = MongoClient(mongo_uri)
 
-
-client = MongoClient(os.environ['MONGO_URI'])
-db = client['your-database-name']
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run()
 
 
 
