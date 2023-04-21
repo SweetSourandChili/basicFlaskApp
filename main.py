ff49867e78f729166dee9862a58b55c058ce49cd
Arda Meriç <ardam416@gmail.com>
2023-04-21 18:35:15 +0300

    ssl check

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ mongo_uri = os.environ['MONGO_URI']
 
 app = fl.Flask(__name__, static_folder='static')
 client = pm.MongoClient(mongo_uri)
+db = client['webData']
+
 
 @app.route('/')
 def index():
@@ -20,9 +22,26 @@ def loginUser():
     return fl.jsonify({'success': True, 'message': 'Login successful'}), 200
 
 
+@app.route('/loginAdmin', methods=['POST'])
+def loginAdmin():
+    username = fl.request.form['username']
+    password = fl.request.form['password']
+    # TODO: check to send token here
+    return fl.jsonify({'success': True, 'message': 'Login successful'}), 200
+
+
+@app.route('/getData', methods=['POST'])
+def getData():
+    print("here")
+    try:
+        db.command('ping')
+        print("here")
+        return fl.jsonify({'success': True, 'message': 'Login successful'}), 200
+    except Exception as e:
+        print(str(e))
+        return fl.jsonify({'success': False, 'message': 'Login successful'}), 505
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
-
-
-
--- static/js/buttonControl.js
+++ static/js/buttonControl.js
@@ -18,8 +18,9 @@ adminLogin.addEventListener("click", function() {
 });
 
 getUser.addEventListener("click", function() {
-     const username = document.getElementById('username').value;
-     const password = document.getElementById('password').value;
+     let inp = document.getElementsByTagName("input");
+     const username = inp[0].value;
+     const password = inp[1].value;
      alert("here");
 
      const xhr = new XMLHttpRequest();
@@ -37,6 +38,38 @@ getUser.addEventListener("click", function() {
 });
 
 getAdmin.addEventListener("click", function() {
+     let inp = document.getElementsByTagName("input");
+     const username = inp[0].value;
+     const password = inp[1].value;
+     alert("here");
+
+     const xhr = new XMLHttpRequest();
+     xhr.open('POST', '/loginAdmin');
+     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+     xhr.onload = function() {
+     if (xhr.status === 200) {
+          // login successful
+          window.location.href = "/admin";
+     } else {
+          alert("Username or password is wrong. Please try again.");
+     }
+     };
+     xhr.send(`username=${username}&password=${password}`);
+});
+
+signIn.addEventListener("click", function (){
+
+     const xhr = new XMLHttpRequest();
+     xhr.open('POST', '/getData');
+     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+     xhr.onload = function() {
+     if (xhr.status === 200) {
+          alert("Successful.");
+     } else {
+          alert("Database connection is not successful!!");
+     }
+     };
+     xhr.send("");
 
 });
 
